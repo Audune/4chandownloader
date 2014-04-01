d2bffd5c87ed34fe934b702d4b65d6dd227fa5e1
Audune <auduneadolynn@gmail.com>
2014-03-31 19:27:28 -0700

    Update Board.cs
    Update 3-31-14
    	Audune:
    			-Fixed the naming issue with the new image content servers.  Now downloads properly again.
    			-Increased the Image download threads from 5 to 10, because 'Murica
    			-Console now prints what Thread it is downloading from, rather than just the generic server that the image lives on
    			-Possibly other tweaks I forgot about, at least it works again.

--- Downloader/Board.cs
+++ Downloader/Board.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.IO;
@@ -35,12 +35,12 @@ namespace Downloader
             set { _directoryInfo = value; }
         }
 
-        private const string ThreadImageRegex = "\\d{13}.(jpg|png|gif)"; // contains 13 digits followed by a . and ends in a jpg or png or gif
-        private const string ThreadLinkRegex = "<a href=\"res/\\d{1,12}\" class=\"replylink\" >Reply</a>";
+        private const string ThreadImageRegex = "\\d{13}.(jpg|png|gif)"; // contains 13 digits followed by a . and ends in a jpg or png or gif - All images currently are 13 digits long, adjust later if it changes
+        private const string ThreadLinkRegex = "<a href=\"res/\\d{1,12}\" class=\"replylink\">Reply</a>"; //Finds Threads via the "Reply" button on the page
         private const string ThreadNameRegex = "\\d{1,12}"; // contains digits 1 to 12 long
         private string ImageLinkRegex
         {
-            get { return "<a href=\"//images.4chan.org/" + Name + "/src/\\d{10,16}.(jpg|png|gif)\" target=\"_blank\">"; }
+            get { return "<a class=\"fileThumb\" href=\"//i.4cdn.org/" + Name + "/src/\\d{10,16}.(jpg|png|gif|jpeg)\" target=\"_blank\">"; } //I dont know why it searches for 10-16 digits when we already search for 13...
         }
 
         public Board(string name = "", bool repeat = false, int maxPage = 5, bool createFolders = true)
@@ -53,27 +53,27 @@ namespace Downloader
             Threads = new ConcurrentBag<string>();
         }
 
-        public void GatherThreads()
+        public void GatherThreads()  //Reads threads up to MaxThread variable amount.  Takes Fourm threads it finds via "Reply" and adds them to the list
         {
             // Page 0 -> Page 15
             Parallel.For(fromInclusive: 0, toExclusive: MaxPage, body: (i, a) =>
             {
                 try
                 {
-                    Console.WriteLine(string.Format("STGR http://boards.4chan.org/{0}/{1}", Name, i));
-                    string html = new WebClient().DownloadString(string.Format("http://boards.4chan.org/{0}/{1}", Name, i));
-                    var matchCollection = Regex.Matches(html, ThreadLinkRegex);
-                    matchCollection.Cast<Match>().ToList().ForEach(thread => Threads.Add(Regex.Match(thread.Value, ThreadNameRegex).Value));
-                    Console.WriteLine(string.Format("FNGT http://boards.4chan.org/{0}/{1}", Name, i));
+                    Console.WriteLine(string.Format("STGR http://boards.4chan.org/{0}/{1}", Name, i));  //Text to Console telling you wtf its doing
+                    string html = new WebClient().DownloadString(string.Format("http://boards.4chan.org/{0}/{1}", Name, i));  //goes to Board, finds thread
+                    var matchCollection = Regex.Matches(html, ThreadLinkRegex);  //Finds shit defined under ThreadLinkRegex
+                    matchCollection.Cast<Match>().ToList().ForEach(thread => Threads.Add(Regex.Match(thread.Value, ThreadNameRegex).Value));  //Adds Threads found to list
+                    Console.WriteLine(string.Format("FNGT http://boards.4chan.org/{0}/{1}", Name, i));  //Writes to Console it is/was reading the Board index of threads
                 }
                 catch(Exception e)
                 {
-                    Console.WriteLine(string.Format("FTGT http://boards.4chan.org/{0}/{1}][{2}", Name, i, e.Message));
+                    Console.WriteLine(string.Format("FTGT http://boards.4chan.org/{0}/{1}][{2}", Name, i, e.Message));  //Exception to catch something, some Error thing, prolly 404 stuff
                 }     
             });
         }
 
-        public void ProcessThreads()
+        public void ProcessThreads()  //I think this is to make Folders in the download directory, where Images will end up going???  Based on Board/Thread name.
         {
             var threadFolders = Threads
                 .Select(thread => new BoardThread()
@@ -86,7 +86,7 @@ namespace Downloader
             BoardThreads = new List<BoardThread>(threadFolders);
 
             var groups = BoardThreads.Select((x, i) => new { Index = i, Value = x })
-                    .GroupBy(x => x.Index / 5) // Sets of 5 would be nice...
+                    .GroupBy(x => x.Index / 5) // Change value to change how many folders it makes at a time?  Currently set to 5.
                     .Select(x => x.Select(v => v.Value).ToList()).ToList();
 
             foreach(var group in groups)
@@ -98,19 +98,19 @@ namespace Downloader
         }
 
 
-        private void ProcessThread(BoardThread thread)
+        private void ProcessThread(BoardThread thread)  //Reads the Boards thread looking at images to download
         {
             try
             {
-                string html = new WebClient().DownloadString(address: new Uri(string.Format(@"http://boards.4chan.org/{0}/res/{1}", Name, thread.Name)));
+                string html = new WebClient().DownloadString(address: new Uri(string.Format(@"http://boards.4chan.org/{0}/res/{1}", Name, thread.Name)));  //Reading....
 
-                MatchCollection links = Regex.Matches(html, ImageLinkRegex, RegexOptions.IgnoreCase);
+                MatchCollection links = Regex.Matches(html, ImageLinkRegex, RegexOptions.IgnoreCase);  //Find stuff that matches rules in ImageLinkRegex
 
-                if(MinImages.HasValue)
+                if(MinImages.HasValue)  //If enabled, wont download from threads with less than a certain number of images
                 {
                     if(links.Count < MinImages.Value)
                     {
-                        Console.WriteLine(string.Format(@"DNMF http://boards.4chan.org/{0}/res/{1}", Name, thread.Name));
+                        Console.WriteLine(string.Format(@"DNMF http://i.4cdn.org/{0}/src/{1}", Name, thread.Name));
 
                         if(!thread.DirectoryInfo.Equals(DirectoryInfo))
                             thread.DirectoryInfo.Delete();
@@ -120,12 +120,12 @@ namespace Downloader
                 }
 
                 var groups = links.Cast<Match>().AsParallel().Select((x, i) => new { Index = i, Value = x })
-                    .GroupBy(x => x.Index / 5) // Sets of 10 would be nice...
+                    .GroupBy(x => x.Index / 10)
                     .Select(x => x.Select(v => v.Value).ToList()).ToList();
 
                 foreach (var group in groups)
                 {
-                    Console.WriteLine("Processing the next 5 images...");
+                    Console.WriteLine("Processing the next 10 images...");
                     // Send in a set of 10 at once...
                     Parallel.ForEach(group, link =>
                     {
@@ -133,28 +133,28 @@ namespace Downloader
 
                         try
                         {
-                            Console.WriteLine(string.Format(@"DLNG http://images.4chan.org/{0}/src/{1}", Name, image));
-                            new WebClient().DownloadFile(new Uri(string.Format(@"http://images.4chan.org/{0}/src/{1}", Name, image)), thread.DirectoryInfo.FullName + "\\" + image);
+                            Console.WriteLine(string.Format(@"DLNG http://boards.4chan.org/{0}/src/{1}/{2}", Name, thread.Name, image));
+                            new WebClient().DownloadFile(new Uri(string.Format(@"http://i.4cdn.org/{0}/src/{1}", Name, image)), thread.DirectoryInfo + "\\" + image);
                         }
-                        catch (WebException e)
+                        catch (WebException e)  //Example:404 prints to the console that the image could not be downloaded in time
                         {
                             var httpWebResponse = e.Response as HttpWebResponse;
 
                             if (httpWebResponse != null)
                             {
-                                Console.WriteLine(string.Format(@"FTDL http://images.4chan.org/{0}/src/{1} - {2}", Name, image, (int)(httpWebResponse).StatusCode));
+                                Console.WriteLine(string.Format(@"FTDL http://i.4cdn.org/{0}/src/{1} - {2}", Name, image, (int)(httpWebResponse).StatusCode));
                             }
                         }
                     });
                 }
             }
-            catch (WebException e)
+            catch (WebException e) //Example:404 prints to the console that the image could not be downloaded in time
             {
                 var httpWebResponse = e.Response as HttpWebResponse;
 
                 if (httpWebResponse != null)
                 {
-                    Console.WriteLine(string.Format(@"FTDL http://boards.4chan.org/{0}/res/{1} - {2}", Name, thread.Name, (int)(httpWebResponse).StatusCode));
+                    Console.WriteLine(string.Format(@"FTDL http://i.4cdn.org/{0}/src/{1} - {2}", Name, thread.Name, (int)(httpWebResponse).StatusCode));
                 }
             }
 
